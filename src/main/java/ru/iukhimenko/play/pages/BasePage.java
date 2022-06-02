cfd2095c6a9ab0461cf0d094273a424efb4b25ce
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-06-02 21:59:59 +0300

    Added menu opening mehtods

--- src/main/java/ru/iukhimenko/play/pages/BasePage.java
+++ src/main/java/ru/iukhimenko/play/pages/BasePage.java
@@ -1,11 +1,20 @@
 package ru.iukhimenko.play.pages;
 
 import ru.iukhimenko.play.elements.NavigationMenu;
+import ru.iukhimenko.play.pages.catalog.ForBabysCatalogPage;
+
+import static ru.iukhimenko.play.enums.NavigationMenuItem.FOR_BABYS;
 
 public abstract class BasePage {
     protected final NavigationMenu navigationMenu;
 
     public BasePage() {
         navigationMenu = new NavigationMenu();
+    }
+
+
+    public ForBabysCatalogPage openForBabysCatalog() {
+        navigationMenu.openMenuItem(FOR_BABYS);
+        return new ForBabysCatalogPage();
     }
 }

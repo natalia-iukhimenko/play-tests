7f1d098b489a55471bcfec8dacc82bd0c21e160a
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2022-06-03 21:56:14 +0300

    Added BaseUITest class

---
+++ src/main/java/ru/iukhimenko/play/core/BaseUITest.java
@@ -0,0 +1,18 @@
+package ru.iukhimenko.play.core;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import static ru.iukhimenko.play.core.ProjectConfiguration.CONFIGURATION;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class BaseUITest {
+    @BeforeAll
+    public void setUp() {
+        Configuration.baseUrl = CONFIGURATION.baseUrl();
+        Selenide.open("/");
+        Selenide.webdriver().object().manage().window().maximize();
+    }
+}
